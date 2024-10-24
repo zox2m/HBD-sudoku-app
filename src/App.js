e71a96544c50b45ae385cb80201dd4b0c76a628d
zox2m <zox2mm@gmail.com>
2024-10-24 22:09:05 +0900

    music

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import BoardView from './components/BoardView';
 import Navbar from './components/Navbar';
 import BoardState from './context/BoardState';
@@ -5,12 +6,17 @@ import BoardState from './context/BoardState';
 function App() {
   return (
     <>
-    <Navbar />
-    <div className='App'>
-      <BoardState>
-        <BoardView />
-      </BoardState>
-    </div>
+      <Navbar />
+      <div className='App'>
+        <BoardState>
+          <BoardView />
+        </BoardState>
+        {/* 배경음악 자동 재생 및 반복 */}
+        <audio autoPlay loop>
+          <source src={`${process.env.PUBLIC_URL}/assets/background-music.mp3`} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
     </>
   );
 }
